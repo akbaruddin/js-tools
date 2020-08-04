218d04b52c0be800a8148571ffbea701fdd99590
Akbar <akbaruddin@live.in>
2020-08-04 12:37:29 +0530

    checkVisible function

---
+++ src/visible.js
@@ -0,0 +1,8 @@
+const checkVisible = elm => {
+  var rect = elm.getBoundingClientRect();
+  var viewHeight = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight
+  );
+  return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+};
